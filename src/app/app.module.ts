a1f4a0c3f60ec9c11fcd659580d5998ec9787537
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-06-16 19:17:50 +0300

    displays employee names on the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [HttpClientModule,
     BrowserModule
   ],
   providers: [],
